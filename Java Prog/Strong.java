761a04b353dda0a22d8f3841c7d5431b43e7cd25
unknown <manjunathragati68@gmail.com>
2026-04-02 23:05:43 +0530

    it is AramStrong number

--- Java Prog/Strong.java
+++ Java Prog/Strong.java
@@ -1,26 +1,26 @@
 public class Strong {
     public static void main(String[] args) {
 
-        int num = 145;
+        int num = 153;
         int original = num;
         int sum = 0;
+        int count=0;
+        while(num !=0){
+            num/=10;
+            count++;
+        }
+        num = original;
 
         while (num != 0) {
             int digit = num % 10;
-
-            int fact = 1;
-            for (int i = 1; i <= digit; i++) {
-                fact = fact * i;
-            }
-
-            sum = sum + fact;
+            sum = sum + (int)Math.pow(digit,count);
             num = num / 10;
         }
-
+System.out.println(sum);
         if (sum == original) {
-            System.out.println(" is a Strong Number");
+            System.out.println(" is a Aramstrong Number");
         } else {
-            System.out.println( " is not a Strong Number");
+            System.out.println( " is not a AramStrong Number");
         }
     }
 }
