e7abc0fa1571d1ded79770c3d9351075e9baf90f
unknown <manjunathragati68@gmail.com>
2026-04-03 23:45:13 +0530

    it is simple number

---
+++ TurSmall.java
@@ -0,0 +1,11 @@
+public class TurSmall {
+			
+		static int isLarge(int a,int b) {
+			int res=a<b? a:b;
+			return res;
+		}
+public static void main(String[] args) {
+	System.out.println(isLarge(2,3));
+}
+	}
+
