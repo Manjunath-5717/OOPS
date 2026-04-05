64a3dc49822fad59bbcb8d1f47702213fa93a264
Manjunath-5717 <manjunathragati704@gmail.com>
2026-04-05 21:54:43 +0530

    array merging using thrid variable

---
+++ ArrayMerge.java
@@ -0,0 +1,19 @@
+public class ArrayMerge {
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5};
+        int[] b = {4, 6, 8};
+        int[] c = new int[a.length + b.length];
+
+        
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            c[a.length + i] = b[i];
+        }
+        for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i] + " ");
+        }
+        System.out.println();
+    }
+}
