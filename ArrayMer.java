7efb12d5130bebc59b5c268e70bafa7f9b962934
Manjunath-5717 <manjunathragati704@gmail.com>
2026-04-05 22:02:14 +0530

    merge two array without using second array

---
+++ ArrayMer.java
@@ -0,0 +1,14 @@
+public class ArrayMer {
+        public static void main(String[] args) {
+        int[] a = {2, 3, 5};
+        int[] b = {4, 6, 8};
+        
+
+        
+      
+        for (int i = 0; i <a.length; i++) {
+            System.out.print(a[i] + "  "+ b[i]+" ");
+        }
+        System.out.println();
+    }
+}
