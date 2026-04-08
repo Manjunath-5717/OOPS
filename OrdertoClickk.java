02e8cf08bde2cdca5fbdb9a25583796a3426f7da
Manjunath-5717 <manjunathragati704@gmail.com>
2026-04-08 23:21:00 +0530

    this program demonstrate how run time polymorphism works

---
+++ OrdertoClickk.java
@@ -0,0 +1,42 @@
+ class Swiggy {
+ void order() {
+	 System.out.println("oreder from swiggy");
+ }
+}
+
+class idli extends Swiggy{
+	void order() {
+		System.out.println("oreder idli");
+	}
+}
+
+class chapati extends Swiggy{
+	void order() {
+		System.out.println("order chapati");
+	}
+}
+
+class dosa extends Swiggy{
+	void order() {
+		System.out.println("oreder dosa");
+	}
+}
+
+public class OrdertoClickk{
+	static void order(Swiggy s1) {
+		s1.order();
+	}
+    
+    public static void main(String[] args) {
+	idli i1=new idli();
+	chapati c1=new chapati();
+	dosa d1=new dosa();
+
+	order(i1);
+	order(c1);
+	order(d1);
+}
+}
+
+
+
