0144f66581dc8753e634fda6254b5e8d6a7cacf1
Manjunath-5717 <manjunathragati704@gmail.com>
2026-04-09 22:52:05 +0530

    it is used to sum the given number

---
+++ SumNU.java
@@ -0,0 +1,18 @@
+public class SumNU {
+    
+    static int countt(int num){
+        int sum=0;
+        while(num!=0){
+            int digit=num%10;
+            sum=sum+digit;
+            num=num/10;
+
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n=countt(145);
+        System.out.println(n);
+    }
+}
