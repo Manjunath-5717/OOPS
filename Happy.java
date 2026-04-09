95cc7a4a3b2fd3f216967f5b56d0fe14abf95341
Manjunath-5717 <manjunathragati704@gmail.com>
2026-04-09 16:58:54 +0530

    it is a simple java program,

---
+++ Happy.java
@@ -0,0 +1,26 @@
+public class Happy{
+    public static void main(String[] args) {
+        int n=19;
+        if(n==1 || n==7){
+            System.out.println("it is happy number");
+        }
+        else if(n>9){
+            int sum=0;
+            while(n>9){
+                sum=0;
+                while(n!=0){
+                    int digit=n%10;
+                    sum=sum+digit*digit;
+                    n=n/10;
+                }
+                n=sum;
+            }
+                if(n==1 || n==7){
+                    System.out.println("it is a happy number");
+                }else{
+                    System.out.println("it is not a happy number");
+                }
+            
+        }
+    }
+}
