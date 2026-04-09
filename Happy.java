110a7b1012cb74f4e4582be893c5ec6d4a53780c
Manjunath-5717 <manjunathragati704@gmail.com>
2026-04-09 22:42:33 +0530

    it is used to check a given number is happy or not

--- Happy.java
+++ Happy.java
@@ -1,26 +1,31 @@
-public class Happy{
-    public static void main(String[] args) {
-        int n=19;
-        if(n==1 || n==7){
-            System.out.println("it is happy number");
-        }
-        else if(n>9){
-            int sum=0;
-            while(n>9){
-                sum=0;
-                while(n!=0){
-                    int digit=n%10;
-                    sum=sum+digit*digit;
-                    n=n/10;
-                }
-                n=sum;
+public class Happy {
+
+    static String happy(int num) {
+
+        while (true) {
+
+            if (num == 1 || num == 7) {
+                return "It is Happy Number";
+            } else if (num < 10) {
+                return "It is NOT Happy Number";
             }
-                if(n==1 || n==7){
-                    System.out.println("it is a happy number");
-                }else{
-                    System.out.println("it is not a happy number");
-                }
-            
+
+            int sum = 0;
+
+            while (num != 0) {
+                int digit = num % 10;
+                sum = sum + digit * digit;
+                num = num / 10;
+            }
+
+            num = sum;
         }
+    }
+
+    public static void main(String[] args) {
+        int n = 19;
+
+        String result = happy(n);
+        System.out.println(result);
     }
 }
