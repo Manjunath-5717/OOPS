884dbcd0b2ff91510d958a98b00699e834a13980
Manjunath-5717 <manjunathragati704@gmail.com>
2026-04-25 21:15:48 +0530

    updated code

--- Primee.java
+++ Primee.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
+
 public class Primee {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number");
-        int num=sc.nextInt();
        
+       int num=11;
         int count=0;
         for(int i=1;i<=10;i++){
             if(num%i==0){
